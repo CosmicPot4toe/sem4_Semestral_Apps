f1582508195450a23826249a13909976135ac1d5
CosmicPot4toe <ji.figueroa.lopez@gmail.com>
2023-08-29 17:04:59 -0400

    Shit is weird

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sem4_Semestral_Apps',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
