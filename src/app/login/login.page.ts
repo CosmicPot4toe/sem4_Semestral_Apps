aa974854c0af726a9c2982755bdb05b7226632c3
CosmicPot4toe <ji.figueroa.lopez@gmail.com>
2023-09-14 15:44:48 -0300

    login load anim
    yee

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../app-model';
+import { AnimationController, Animation  } from '@ionic/angular';
 @Component({
 	selector: 'app-login',
 	templateUrl: './login.page.html',
 	styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-	constructor(private router:Router) { }
+
+	
+	@ViewChild('page',{ read : ElementRef }) page!:ElementRef;
+	private anim!:Animation
+
+	constructor(private router:Router,private animCtrl:AnimationController) { }
 	usuarios:User[]=[
 		{id:1,nombre:'pepito',pass:'1234'},
 		{id:2,nombre:'Alejandro',pass:'4321'},
@@ -17,15 +23,24 @@ export class LoginPage implements OnInit {
 	user=""
 	pw=""
 
+	ngAfterViewInit(){
+		this.anim = this.animCtrl
+			.create()
+			.addElement(this.page.nativeElement)
+			.duration(1000)
+			.fromTo('opacity', '1', '0.01');
+	}
 	
 	contador= 0;
-	validarLogin(){
+	async validarLogin(){
 		for(const x of this.usuarios ){
 			if(x.nombre===this.user){
 				if(x.pass ===this.pw){
 					this.contador = this.contador+1;
 					console.log("usuario y pw correctos! bienvenido")
-					this.router.navigate(['/home', this.user]);//si usuario y pw son correctos
+					await this.anim.play();
+					await this.router.navigate(['/home', this.user]);//si usuario y pw son correctos
+					this.anim.stop();
 					//reset fields
 					this.pw="";
 					this.user="";
@@ -36,7 +51,7 @@ export class LoginPage implements OnInit {
 			console.log("usuario y/o password incorrectos!");
 		}
 	}
-
+	
 	ngOnInit() {
 	}
 
