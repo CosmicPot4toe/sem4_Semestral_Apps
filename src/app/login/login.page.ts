397f8cd5cb3e8e82cccbe60d40c50b8b25e337f2
CosmicPot4toe <ji.figueroa.lopez@gmail.com>
2023-09-14 16:03:31 -0300

    shit
    name of var is now load
    -we get a flash of background, dk what to do about it

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -11,24 +11,25 @@ export class LoginPage implements OnInit {
 
 	
 	@ViewChild('page',{ read : ElementRef }) page!:ElementRef;
-	private anim!:Animation
-
+	private load!:Animation
+	
 	constructor(private router:Router,private animCtrl:AnimationController) { }
 	usuarios:User[]=[
 		{id:1,nombre:'pepito',pass:'1234'},
 		{id:2,nombre:'Alejandro',pass:'4321'},
 		{id:3,nombre:'Renato',pass:'0001'},
 	];
+
 	dato!:String;
 	user=""
 	pw=""
 
 	ngAfterViewInit(){
-		this.anim = this.animCtrl
+		this.load = this.animCtrl
 			.create()
 			.addElement(this.page.nativeElement)
 			.duration(1000)
-			.fromTo('opacity', '1', '0.01');
+			.fromTo('opacity', '1', '0');
 	}
 	
 	contador= 0;
@@ -38,12 +39,12 @@ export class LoginPage implements OnInit {
 				if(x.pass ===this.pw){
 					this.contador = this.contador+1;
 					console.log("usuario y pw correctos! bienvenido")
-					await this.anim.play();
+					await this.load.play();
 					await this.router.navigate(['/home', this.user]);//si usuario y pw son correctos
-					this.anim.stop();
 					//reset fields
 					this.pw="";
 					this.user="";
+					this.load.stop();
 				}
 			}
 		}
