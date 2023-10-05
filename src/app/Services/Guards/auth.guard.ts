f18f8521151dd815fd771c8418d5ab3615d8b4d2
NMPotato <ji.figueroa.lopez@gmail.com>
2023-10-05 11:39:19 -0300

    Update auth.guard.ts
    tes

--- src/app/Services/Guards/auth.guard.ts
+++ src/app/Services/Guards/auth.guard.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-import { AuthService } from '../fb/auth.service';
+// import { AuthService } from '../fb/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(public authService:AuthService){}
+  constructor(){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
