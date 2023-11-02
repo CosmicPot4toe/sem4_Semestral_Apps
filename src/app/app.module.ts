710e2a7a2d0ae75b4885579f1925a1837c2b6447
NMPotato <ji.figueroa.lopez@gmail.com>
2023-11-02 11:55:28 -0300

    getting there?
    idk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -20,7 +22,7 @@ import { AppRoutingModule } from './app-routing.module';
 		AngularFireModule,AngularFireAuthModule,
 		AngularFireModule.initializeApp(environment.firebaseConfig)
 	],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },BarcodeScanner],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
