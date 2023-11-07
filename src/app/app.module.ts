f966cb7bbcebe1406877a3cb1838b0ce7a2c8901
NMPotato <ji.figueroa.lopez@gmail.com>
2023-11-07 08:59:40 -0300

    Revert "getting there?"
    This reverts commit 710e2a7a2d0ae75b4885579f1925a1837c2b6447.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,6 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -22,7 +20,7 @@ import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 		AngularFireModule,AngularFireAuthModule,
 		AngularFireModule.initializeApp(environment.firebaseConfig)
 	],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },BarcodeScanner],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
